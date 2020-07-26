0f4bd1e58b3c78a7aea7f0bf73061a4f447910d2
Fatimah <daviesfatimah97@gmail.com>
2020-07-26 12:08:36 -0700

    include form to get name data

--- firstTask/server.js
+++ firstTask/server.js
@@ -2,10 +2,18 @@
 const http = require('http')
 //initialize port number
 const port = 8080
+const fs = require('fs')
 
 //create a server
 http.createServer(function(req, res){
-    //display the string
-    res.end("Hello World, Welcome to WeJapa Internships")
+    //http header (response to be displayed as html)
+    res.writeHead(200, {'Content-Type': "text/html"});
+
+    //read index.html file to display the form
+    fs.readFile('index.html', (err, data)=>{
+        res.write(data);
+        //end the respone
+        res.end();
+    });
     //listen on port 8080
 }).listen(port)
